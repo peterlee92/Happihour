d1abc5ed0340c58c079d523cf5c8e2220f6da501
KaanDarcey <kaanhdarcey@gmail.com>
2019-10-06 15:12:23 -0700

    Created Favourite/List item and FavItems test file

---
+++ comps/FavItems.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+
+function FavItems() {
+    return(
+        <TouchableOpacity>
+            <View style={}>
+                <View>
+                    <Image style={} source={require('')}/>
+                </View>
+                <View>
+                    <Text style={}>Location Name</Text>
+                    <Text style={}>Location Description</Text>
+                    <Image style={} source={require('')}/>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+export default FavItems;
