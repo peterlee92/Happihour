218fd9e8c712787c24c024fe5a7ff77be8f84566
KaanDarcey <kaanhdarcey@gmail.com>
2019-10-07 08:32:18 -0700

    added UseState changes for App.js and MenuBar

--- App.js
+++ App.js
@@ -6,15 +6,34 @@
  * @flow
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, Image} from 'react-native';
 import styles from './styles/AppStyle';
 import LoginForm from './comps/LoginForm';
+import GetHome from './comps/Pages/GetHome';
+import ListPage from './comps/Pages/ListPage';
+import FavPage from './comps/Pages/FavPage';
 
 function App() {
+  // ChangePage changes the page based on variables passed through.
+  // variables passed through are done so via events such as menu
+  const [ChangePage, setChangePage] = useState(0);
+
+  var page = <LoginForm />;
+
+  if(ChangePage == 1){
+    page = <GetHome />
+  } else 
+  if(ChangePage == 3){
+    page = <ListPage />
+  } else
+  if(ChangePage == 4){
+    page = <FavPage />
+  }
+
   return (
     <View style={styles.container}>
-      <LoginForm />
+      {page}
     </View>
   );
 };
--- comps/MenuBar.js
+++ comps/MenuBar.js
@@ -4,28 +4,38 @@ import {View, Text, Image, TouchableOpacity} from 'react-native';
 function MenuBar(){
     return(
         <View style={}>
-            <TouchableOpacity onPress={}>
+            // setChangePage is usestate that will change the page based on
+            // the passed in value from event. usestate is located on app.js
+            <TouchableOpacity onPress={()=>{
+                setChangePage(ChangePage = 1);
+            }}>
                 <View style={}>
                     <Image style={} source={require('')}/>
                     <Text style={}>Map</Text>
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={}>
+            <TouchableOpacity onPress={()=>{
+                setChangePage(ChangePage = 3);
+            }}>
                 <View style={}>
                     <Image style={""} source={require('')}/>
                     <Text style={}>List</Text>
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={}>
+            <TouchableOpacity onPress={()=>{
+                setChangePage(ChangePage = 1);
+            }}>
                 <View style={}>
                     <Image style={""} source={require('')}/>
                     <Text style={}>Transport</Text>
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={}>
+            <TouchableOpacity onPress={()=>{
+                setChangePage(ChangePage = 4);
+            }}>
                 <View style={}>
                     <Image style={""} source={require('')}/>
                     <Text style={}>Favourites</Text>
