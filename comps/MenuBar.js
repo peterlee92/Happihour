78b88a9c9fe5253b0630d643b29f74227a629eb9
KaanDarcey <kaanhdarcey@gmail.com>
2019-10-07 18:27:42 -0700

    Added Icons to MenuBar

--- comps/MenuBar.js
+++ comps/MenuBar.js
@@ -10,7 +10,7 @@ function MenuBar(){
                 setChangePage(ChangePage = 1);
             }}>
                 <View style={}>
-                    <Image style={} source={require('')}/>
+                    <Image style={} source={require('../Mockups + Comps/Icons/Nav Bar Icons/Greyed Icons/location_icon.png')}/>
                     <Text style={}>Map</Text>
                 </View>
             </TouchableOpacity>
@@ -19,7 +19,7 @@ function MenuBar(){
                 setChangePage(ChangePage = 3);
             }}>
                 <View style={}>
-                    <Image style={""} source={require('')}/>
+                    <Image style={""} source={require('../Mockups + Comps/Icons/Nav Bar Icons/Greyed Icons/list_icon.png')}/>
                     <Text style={}>List</Text>
                 </View>
             </TouchableOpacity>
@@ -28,7 +28,7 @@ function MenuBar(){
                 setChangePage(ChangePage = 1);
             }}>
                 <View style={}>
-                    <Image style={""} source={require('')}/>
+                    <Image style={""} source={require('../Mockups + Comps/Icons/Nav Bar Icons/Greyed Icons/car_icon.png')}/>
                     <Text style={}>Transport</Text>
                 </View>
             </TouchableOpacity>
@@ -37,7 +37,7 @@ function MenuBar(){
                 setChangePage(ChangePage = 4);
             }}>
                 <View style={}>
-                    <Image style={""} source={require('')}/>
+                    <Image style={""} source={require('../Mockups + Comps/Icons/Nav Bar Icons/Greyed Icons/fav_icon.png')}/>
                     <Text style={}>Favourites</Text>
                 </View>
             </TouchableOpacity>
