3b6f476d537953a7a6d8a7cd4c048fa36d5074d5
Peter <mhyeok92@gmail.com>
2019-10-20 20:41:34 -0700

    Reactivated Router

--- Route.js
+++ Route.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Router, Stack, Scene} from 'react-native-router-flux';
 
-//@@ import screens here
+// import screens here
 import Login from './screens/Login';
 import Register from './screens/Register';
+import Detail from './screens/Detail';
 
 
 function Route(){
@@ -12,6 +13,7 @@ function Route(){
             <Stack key="root" hideNavBar={true}>
             <Scene key="login" component={Login} title="Login" initial={true}/>
             <Scene key="register" component={Register} title="Register" />
+            <Scene key="detail" component={Detail} title="Detail" />
         </Stack>
         </Router>
     )
