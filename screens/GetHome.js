f143f1846e2ec50d65db68c345fa19653c39bbcd
KaanDarcey <kaanhdarcey@gmail.com>
2019-10-26 13:58:38 -0700

    redesigned gethome page with functionality

--- screens/GetHome.js
+++ screens/GetHome.js
@@ -2,32 +2,40 @@ import React from 'react';
 import {Text, View, Image, TouchableOpacity} from 'react-native';
 import MenuBar from '../comps/MenuBar';
 import style from '../styles/ScreenStyles/GetHomeStyle';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {Actions} from 'react-native-router-flux';
 
 function GetHome(){
     return(
         <View style={style.container}>
-            <Text style={style.GHS}>Let's Get You Home Safe</Text>
+            <Text style={[style.GHS, {marginBottom:30}]}>Let's Get You Home Safe</Text>
             <View style={style.rows}>
-                <TouchableOpacity style={style.position}>
-                    <Image style={style.Img}/>
-                    <Text style={style.GHS}>Transit</Text>
+                <TouchableOpacity style={[style.position, {backgroundColor:'#234378'}]}>
+                    <View style={style.img}>
+                        <FontAwesomeIcon icon="bus-alt" size={100} color="white" style={style.Img}/>
+                    </View>
+                    <Text style={style.GHS}>TRANSIT</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={style.position}>
-                    <Image style={style.Img}/>
-                    <Text style={style.GHS}>Taxi</Text>
+                <TouchableOpacity style={[style.position, {backgroundColor:'#D1C74B'}]} onPress={()=>(Actions.taxi())}>
+                    <View style={style.img2}>
+                        <FontAwesomeIcon icon="taxi" size={100} color="white" style={style.Img2}/>
+                    </View>
+                    <Text style={style.GHS}>TAXI</Text>
                 </TouchableOpacity>
             </View>
             <View style={style.rows}>
-                <TouchableOpacity style={style.position}>
-                    <Image style={style.Img}/>
-                    <Text style={style.GHS}>Call A Friend</Text>
+                <TouchableOpacity style={[style.position, {backgroundColor:'#9D403C'}]}>
+                    <View style={style.img3}>
+                        <FontAwesomeIcon icon="user" size={100} color="white" style={style.Img3}/>
+                    </View>
+                    <Text style={[style.GHS, {marginBottom:-15}]}>EMERGENCY CONTACT</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={style.position}>
+                {/* <TouchableOpacity style={style.position}>
                     <Image style={style.Img}/>
                     <Text style={style.GHS}>911</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
             </View>
             <MenuBar />
         </View>
--- styles/ScreenStyles/GetHomeStyle.js
+++ styles/ScreenStyles/GetHomeStyle.js
@@ -6,20 +6,43 @@ const style = StyleSheet.create({
         justifyContent:"center",
         alignItems:"center",
         backgroundColor:"#061a20",
-        height:"100%"
+        height:"100%",
+        paddingBottom:50
     },
     GHS:{
         color:"white",
-        fontSize:18
+        fontSize:16,
+        fontWeight:"bold",
+        letterSpacing:3,
+        textAlign:'center'
     },
     Img:{
+        width:300,
+        height:150,
+        margin:50,
+        marginBottom:30
+    },
+    Img2:{
+        width:200,
+        height:150,
+        margin:50,
+        marginBottom:30
+    },
+    Img3:{
         width:150,
         height:150,
+        margin:50,
+        marginBottom:30
     },
     position:{
         justifyContent:"center",
         alignItems:"center",
         flexDirection:"column",
+        width:150,
+        height:150,
+        margin:10,
+        marginBottom:40,
+        borderRadius:10
     },
     rows:{
         flexDirection:"row"
