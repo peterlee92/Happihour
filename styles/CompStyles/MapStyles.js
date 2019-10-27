35af7b95082fcadaca56bb20a382e44319fcede3
Peter <mhyeok92@gmail.com>
2019-10-27 12:47:13 -0700

    made mappage and styles

---
+++ styles/CompStyles/MapStyles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'black'
+    },
+    map:{
+        width:'100%',
+        height:'100%'
+    }
+})
+
+export default styles;
---
+++ styles/ScreenStyles/MapPageStyles.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+
+})
+
+export default styles;
