94b33cc515bb9cd4b920114a477332c8c918e7e9
Peter <mhyeok92@gmail.com>
2019-11-13 19:00:43 -0800

    set up backend

--- screens/MapPage.js
+++ screens/MapPage.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Image} from 'react-native';
 import Map from '../comps/Map';
 import MenuBar from '../comps/MenuBar'
 import styles from '../styles/ScreenStyles/MapPageStyles';
+import TimePopup from '../comps/TimePopup';
+import LocationPopup from '../comps/LocationPopup';
 
 
 
-function MapPage(props){
-    const region = {
-        latitude:49.282730,
-        longitude:-123.120735,
-        latitudeDelta:0.09222,
-        longitudeDelta:0.0421
+function MapPage(){
+
+    const [Timepop, setTimepop] = useState(false);
+    const [Locationpop, setLocationpop] = useState(false);
+    const [R_name, setR_name] = useState();
+    const [Tfilter, setTfilter] = useState([]);
+    const [Lfilter, setLfilter] =useState([]);
+
+    var showpopup = null;
+    if(Timepop == true){
+        showpopup = (
+            <TimePopup 
+                setTimepop={setTimepop}
+                setTfilter={setTfilter}
+                setR_name={setR_name}
+            />
+        )
+    }else if(Locationpop == true){
+        showpopup=(
+            <LocationPopup 
+                setLocationpop={setLocationpop} 
+                setLfilter={setLfilter}
+                setR_name={setR_name}
+            />
+        )
+    }else {
+        showpopup = null;
     }
+
+
+
     return(
         <View style={{flex:1}}>
             <Image 
                 style={styles.header}
                 source={require('../imgs/Flow_Header.png')}
             />          
-            {/* <View style={{position:'absolute', elevation:100, width:'100%',justifyContent:'center',alignItems:'center'}}>
-                <SearchBar
-                    style={styles.searchBar}
-                    placeholder="Bar, Restaurant and Pub"
-                    inputContainerStyle={{backgroundColor:'white', height:30,width:'80%',borderWidth:0}}
-                />
-            </View> */}
             <Map 
-                page={props.page}
+                setTimepop={setTimepop}
+                setLocationpop={setLocationpop}
+                Tfilter={Tfilter}
+                Lfilter={Lfilter}
+                R_name={R_name}
             />
             <MenuBar />
+            {showpopup}
         </View>
     )
 }
