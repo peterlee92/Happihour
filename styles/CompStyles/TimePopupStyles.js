c3336791aabfc19e3e5ac00678e491fca9b1942f
Peter <mhyeok92@gmail.com>
2019-11-13 19:00:28 -0800

    styles for locationpopup and timepopup

---
+++ styles/CompStyles/TimePopupStyles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    container:{
+        position:'absolute',
+        justifyContent:'center',
+        alignItems:'center',
+        width:"100%",
+        height:"100%"
+    },
+    wrapper:{
+        backgroundColor:'black',
+        width:"80%",
+        height:"40%",
+        borderRadius:15
+    },
+    scroll:{
+        width:"100%", 
+        height:"100%",
+    },
+    time:{
+        color:"white",
+        fontSize:40,
+        paddingVertical:5
+        
+
+    }
+})  
+
+export default styles;
