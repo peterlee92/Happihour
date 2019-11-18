f7c18922aed70176ad7696dcaae89e14824d6b54
Peter <mhyeok92@gmail.com>
2019-11-18 12:43:38 -0800

    created OnBoarding comps and page

---
+++ comps/OnBoarding1.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import styles from '../styles/CompStyles/OnBoarding1Styles';
+
+function OnBoarding1(){
+    return(
+        // <View style={{justifyContent:'center',alignItems:'center'}}>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.signuptxt}>SIGN UP</Text>
+                <Image 
+                source={require('../icons/signIn.png')}
+                style={styles.phone}
+            />
+            </View>
+            <View style={styles.content}>
+                <Text style={styles.contenttxt}>Sign up with Happihour to view all the best deals at your favourite restaurant, pub or bar.</Text>
+                <TouchableOpacity
+                    style={styles.NextBut}
+                >
+                    <Text style={styles.NextTxt}>Next</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.SkipTxt}>Skip</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        // </View>
+    )
+}
+
+export default OnBoarding1;
